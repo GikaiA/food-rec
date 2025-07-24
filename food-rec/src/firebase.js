2c96c2f4c71f5cfc549c7e7e513300b4127597fa
Gikai Andrews <gnaboys@gmail.com>
2025-07-23 20:34:18 -0400

    added firebase config file

---
+++ food-rec/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPJpCNuCr8zMRdVDCEO0auCJKjoggeiGo",
+  authDomain: "food-55ec2.firebaseapp.com",
+  projectId: "food-55ec2",
+  storageBucket: "food-55ec2.firebasestorage.app",
+  messagingSenderId: "839994028655",
+  appId: "1:839994028655:web:9676f702376973eb29939a",
+  measurementId: "G-CXK568R939"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
