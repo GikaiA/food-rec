ad450ebb6268d10c537a26ad86e1031e8ee033bb
Gikai Andrews <gnaboys@gmail.com>
2025-07-29 21:24:50 -0400

    changed title on index.html and added skeleton code in profile component

--- food-rec/src/Profile/Profile.jsx
+++ food-rec/src/Profile/Profile.jsx
@@ -5,7 +5,23 @@ function Profile() {
   return (
     <div className='profile'>
       <h1>Welcome User!</h1>
-      
+      <div className='profile-section'>
+        <h1>My Information</h1>
+        <div className='information-section'>
+          <p>Email: placeholder</p>
+          <p>Password: placeholder</p>
+          <div className='saved-info-section'>
+            <div className='sub-info'>
+              <h2>Saved Recipes</h2>
+              <p>Placeholder</p>
+            </div>
+            <div className='sub-info'>
+              <h2>Saved Recipes</h2>
+              <p>Placeholder</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
