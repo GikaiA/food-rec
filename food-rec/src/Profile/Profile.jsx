1949520a2b050ce9c782ce09ebd34a2d5f3d6cc6
Gikai Andrews <gnaboys@gmail.com>
2025-08-06 21:29:09 -0400

    added delete button

--- food-rec/src/Profile/Profile.jsx
+++ food-rec/src/Profile/Profile.jsx
@@ -1,50 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import './Profile.css';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase';
+import React, { useEffect, useState } from "react";
+import "./Profile.css";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 function Profile() {
   const [user, setUser] = useState(null);
-  const [firstname, setFirstName] = useState('');
+  const [firstname, setFirstName] = useState("");
 
   useEffect(() => {
-
- 
-   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
         // Get first name from displayName
-        setFirstName(currentUser.displayName || 'User');
+        setFirstName(currentUser.displayName || "User");
       }
     });
 
     return () => unsubscribe();
- })
+  });
   return (
-    <div className='profile'>
+    <div className="profile">
       <h1>Welcome, {firstname}!</h1>
-      <div className='profile-section'>
+      <div className="profile-section">
         <h1>My Information</h1>
-        <div className='information-section'>
+        <div className="information-section">
           <p>Email: {user?.email}</p>
           <p>Password: placeholder</p>
-          <button className='update-button'>
-            Update Profile
-          </button>
-          <div className='saved-info-section'>
-            <div className='sub-info'>
-              <h2>Saved Recipes</h2>
-              <p>Placeholder</p>
-            </div>
-            <div className='sub-info'>
-              <h2>Saved Restaurants</h2>
-              <p>Placeholder</p>
-            </div>
+          <div className="buttons">
+            <button className="update-button">Update Profile</button>
+            <button className="delete-button">Delete Account</button>
+          </div>
+        </div>
+        <div className="saved-info-section">
+          <div className="sub-info">
+            <h2>Saved Recipes</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="sub-info">
+            <h2>Saved Restaurants</h2>
+            <p>Placeholder</p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
