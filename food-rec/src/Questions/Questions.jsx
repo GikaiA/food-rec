fbd7a0f0df5971dcd5d161068c2ce2a53095a525
Gikai Andrews <gnaboys@gmail.com>
2025-09-17 21:12:01 -0400

    updated checkboxes

--- food-rec/src/Questions/Questions.jsx
+++ food-rec/src/Questions/Questions.jsx
@@ -5,37 +5,23 @@ function Questions() {
   return (
     <div className="questions">
       <div className="questions-container">
-        <p>What you feeling today</p>
-      <select name="dropdown" className="dropdown">
-        {" "}
-        <option value="">Select an answer</option>{" "}
-        <option value="value1">label 1</option>{" "}
-        <option value="value2">label 2</option>{" "}
-      </select>
-        <p>What you feeling today</p>
-      <select name="dropdown" className="dropdown">
-        {" "}
-        <option value="">Select an answer</option>{" "}
-        <option value="value1">label 1</option>{" "}
-        <option value="value2">label 2</option>{" "}
-      </select>
-        <p>What you feeling today</p>
-      <select name="dropdown" className="dropdown">
-        {" "}
-        <option value="">Select an answer</option>{" "}
-        <option value="value1">label 1</option>{" "}
-        <option value="value2">label 2</option>{" "}
-      </select>
-        <p>What you feeling today</p>
-      <select name="dropdown" className="dropdown">
-        {" "}
-        <option value="">Select an answer</option>{" "}
-        <option value="value1">label 1</option>{" "}
-        <option value="value2">label 2</option>{" "}
-      </select>
-      <div className="question-submit-button">
-        <button>Submit Answers</button>
-      </div>
+        <h1 className="question">What you feeling today? (Select all that apply) </h1>
+        <div className="checkbox-section">
+          <input type="checkbox" id="breakfast" name="Breakfast" className="checkbox" />
+          <label className="checkbox-label">Breakfast</label>
+        </div>
+        <div className="checkbox-section">
+          <input type="checkbox" id="breakfast" name="Breakfast" className="checkbox" />
+          <label className="checkbox-label">Lunch</label>
+        </div>
+        <div className="checkbox-section">
+          <input type="checkbox" id="breakfast" name="Breakfast" className="checkbox" />
+          <label className="checkbox-label">Dinner</label>
+        </div>
+        <div className="arrow-buttons">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     </div>
   );
